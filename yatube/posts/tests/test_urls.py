933eac479648db83a6bdc8bcc824366a2be4e82f
Nikita Astaforkin <astaforkin90@gmail.com>
2023-01-20 21:37:31 +0300

    add fix test_urls

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -44,6 +44,7 @@ class PostsURLTests(TestCase):
         self.authorized_client_not_authorized.force_login(
             PostsURLTests.user_not_authorized
         )
+        cache.clear()
 
     def test_urls_exists_at_desired_location(self):
         """Проверяем доступность страниц приложения Posts."""
@@ -52,6 +53,8 @@ class PostsURLTests(TestCase):
             f'/group/{self.group.slug}/',
             f'/profile/{self.user.username}/',
             f'/posts/{self.post.pk}/',
+            '/about/author/',
+            '/about/tech/',
         ]
 
         for address in templates_url_names:
@@ -63,8 +66,9 @@ class PostsURLTests(TestCase):
                 self.assertEqual(authorized_response.reason_phrase, 'OK')
 
     def test_post_edit_url_exists_at_desired_location(self):
-        ("""Проверяем доступность страницы редактирования поста """
-         """приложения Posts.""")
+        """
+        Проверяем доступность страницы редактирования поста приложения Posts.
+        """
         address = f'/posts/{self.post.pk}/edit/'
         guest_response = self.guest_client.get(address, follow=True)
         authorized_response = self.authorized_client.get(address)
@@ -108,7 +112,6 @@ class PostsURLTests(TestCase):
 
     def test_urls_uses_correct_template(self):
         """Проверяем шаблоны приложения Posts."""
-        cache.clear()
         for address, template in self.url_templates_names.items():
             with self.subTest(address=address):
                 response = self.authorized_client.get(address)
