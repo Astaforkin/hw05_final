a7418effc2170b356fd37b2c770211981aa1f832
Nikita Astaforkin <astaforkin90@gmail.com>
2023-01-21 23:16:59 +0300

    final review fix posts/admin.py

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Group
+from .models import Post, Group, Follow, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -22,6 +22,6 @@ class GroupAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Post, PostAdmin)
-
-
 admin.site.register(Group, GroupAdmin)
+admin.site.register(Follow)
+admin.site.register(Comment)
