2f2fe476dd75999ea8275041c424e25963f15fc3
Nikita Astaforkin <astaforkin90@gmail.com>
2023-01-22 10:54:39 +0300

    final review fix posts/test_forms.py

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,12 +1,16 @@
 from http import HTTPStatus
-
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
+from django.conf import settings
+import tempfile
 
-from ..models import Group, Post
+from ..models import Group, Post, Comment
 
 User = get_user_model()
+
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
 
 class PostFormTests(TestCase):
@@ -28,27 +32,45 @@ class PostFormTests(TestCase):
         )
 
     def setUp(self):
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
+        )
+        uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
+        self.form_data = {
+            'text': 'Тестовый текст',
+            'group': self.group.id,
+            'image': uploaded,
+        }
         self.guest_user = Client()
         self.authorized_client = Client()
         self.authorized_client.force_login(self.post_author)
 
     def test_authorized_user_create_post(self):
         """Проверка создания записи авторизированным клиентом."""
-        form_data = {
-            'text': 'Текст поста',
-            'group': self.group.id,
-        }
         response = self.authorized_client.post(
             reverse('posts:post_create'),
-            data=form_data,
+            data=self.form_data,
             follow=True
         )
         self.assertEqual(Post.objects.count(), 1)
         post = Post.objects.last()
         self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertEqual(post.text, form_data['text'])
+        self.assertEqual(post.text, self.form_data['text'])
         self.assertEqual(post.author, self.post_author)
-        self.assertEqual(post.group_id, form_data['group'])
+        self.assertEqual(post.group_id, self.form_data['group'])
+        self.assertTrue(Post.objects.filter(
+            id=1,
+            image=post.image
+        ).exists())
 
     def test_authorized_user_edit_post(self):
         """Проверка редактирования записи авторизированным клиентом."""
@@ -79,16 +101,27 @@ class PostFormTests(TestCase):
     def test_not_authorized_user_create_post(self):
         """Проверка создания записи не авторизированным пользователем."""
         posts_count = Post.objects.count()
-        form_data = {
-            'text': 'Текст поста',
-            'group': self.group.id,
-        }
         response = self.guest_user.post(
             reverse('posts:post_create'),
-            data=form_data,
+            data=self.form_data,
             follow=True
         )
         redirect = reverse('login') + '?next=' + reverse('posts:post_create')
         self.assertRedirects(response, redirect)
         self.assertEqual(Post.objects.count(), posts_count)
         self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_add_comment_post(self):
+        """Добавление комментария авторизированным пользователем."""
+        post = Post.objects.create(
+            text='Текст поста для редактирования',
+            author=self.post_author,
+            group=self.group,
+        )
+        comments_count = Comment.objects.count()
+        self.authorized_client.post(
+            reverse('posts:add_comment', kwargs={'post_id': post.id}),
+            data=self.form_data,
+            follow=True)
+        self.assertEqual(Comment.objects.count(), comments_count + 1)
+        self.assertTrue(Comment.objects.filter(id=1).exists())
